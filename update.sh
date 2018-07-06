046b206dfa1a5207b6d40dd29bab3a7380b5f198
little bad <741466738@qq.com>
2018-07-06 22:42:03 +0800

    origin update

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@
 # Created Time: Fri 06 Jul 2018 10:07:39 PM DST
 #########################################################################
 #!/bin/bash
-URL = git@github.com:WuGodLittleBad/$1.git
+URL=git@github.com:WuGodLittleBad/$1.git
 
 git add .
 git commit -m "$2"
