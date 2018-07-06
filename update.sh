da3a639e1bdf6d4700ba072044248b204ce9604f
little bad <741466738@qq.com>
2018-07-06 23:02:09 +0800

    my_git_api

--- update.sh
+++ update.sh
@@ -5,11 +5,8 @@
 # Created Time: Fri 06 Jul 2018 10:07:39 PM DST
 #########################################################################
 #!/bin/bash
-URL=git@github.com:WuGodLittleBad/$1.git
 
 git add .
-git commit -m "$2"
-echo "file will be commited to ${URL}"
+git commit -m "$1"
 
-git remote add origin ${URL}
 git push -u origin master
