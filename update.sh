4bb8b6142fd4530284eb2cd4bd4ec3d455ce9c02
little bad <741466738@qq.com>
2018-07-06 22:39:06 +0800

    origin update

--- update.sh
+++ update.sh
@@ -5,10 +5,11 @@
 # Created Time: Fri 06 Jul 2018 10:07:39 PM DST
 #########################################################################
 #!/bin/bash
-
+URL = git@github.com:WuGodLittleBad/$1.git
 
 git add .
 git commit -m "$2"
-echo "$1"
-git remote add origin git@github.com:WuGodLittleBad//"$1".git
+echo "$(URL)"
+
+git remote add origin $(URL)
 git push -u origin master
